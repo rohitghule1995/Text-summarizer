b7a5b5390f0cb9af7423058a19b86658d0fba905
Rohit Ghule <rohitghule1995@gmail.com>
2023-12-06 00:46:55 +0530

    data_validation stage added

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
 from textSummarizer.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from textSummarizer.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
 from textSummarizer.logging import logger
 
 STAGE_NAME = "Data Ingestion stage"
 
 logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
 data_ingestion = DataIngestionTrainingPipeline()
+data_ingestion.main()
+logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+
+
+STAGE_NAME = "Data validation stage"
+
+logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+data_ingestion = DataValidationTrainingPipeline()
 data_ingestion.main()
 logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
